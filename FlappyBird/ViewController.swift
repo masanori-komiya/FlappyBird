926d46f2fb38da938da935c49ef7068ec71546aa
masanori-komiya <little_temple@icloud.com>
2021-01-06 22:59:40 +0900

    flappy bird

--- FlappyBird/ViewController.swift
+++ FlappyBird/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //SKviewに型を変換する
+        let skView = self.view as! SKView
+        
+        //FPSに表示する
+        skView.showsNodeCount = true
+        
+        //ビューと同じサイズでシーンを作成する
+        let scene = GameScene(size:skView.frame.size)
+        
+        //ビューにシーンを表示する
+        skView.presentScene(scene)
+        
     }
 
 
